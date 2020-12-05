42d61387a1b1049bdbe08e4d014654183850c5c2
Andrew <andrewk745@gmail.com>
2020-12-05 12:48:18 -0500

    added associations

--- migrations/20201205170054-create-to-do.js
+++ migrations/20201205170054-create-to-do.js
@@ -14,6 +14,9 @@ module.exports = {
       task: {
         type: Sequelize.STRING
       },
+      userId: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- models/user.js
+++ models/user.js
@@ -1,25 +1,13 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    email: DataTypes.STRING,
+  const User = sequelize.define("User", {
+    username: DataTypes.STRING,
     password_digest: DataTypes.STRING,
-    username: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
-};
+    email: DataTypes.STRING
+  },{})
+
+  User.associate = function(models) {
+    User.hasMany(models.ToDos, {
+      foreignKey: 'userId'
+    })
+  }
+}
