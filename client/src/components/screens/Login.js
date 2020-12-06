140bf5ee6fa5a5edeb6a19a7892914fdd39f05cd
Andrew <andrewk745@gmail.com>
2020-12-06 18:01:53 -0500

    finished login form

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Title } from '../Shared/Title'
-import { TextField, Button } from '@material-ui/core'
+import { TextField } from '@material-ui/core'
 
 
 
@@ -8,10 +8,10 @@ import { TextField, Button } from '@material-ui/core'
 export default function Login() {
 
 
-    const [ username, setUsername ] = useState('')
-    const [ password, setPassword ] = useState('')
-    const [ email, setEmail ] = useState('')
-    const [ error, setError ] = useState(true)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [error, setError] = useState(true)
 
 
     const usernameChange = (e) => {
@@ -24,17 +24,17 @@ export default function Login() {
         setEmail(e.target.value)
     }
 
-
     return (
         <div class='hero is-fullheight is-primary'>
             <div class='hero-body'>
                 <div class='container is-large has-text-centered'>
                     <div class='box is-flex is-justify-content-center'>
-                        <div class='column is-half'>
+                        <div class='column is-half my-6'>
                             <div class='is-flex is-flex-direction-column'>
                                 <Title
                                     title='Account Login'
                                     color='primary'
+                                    transformation='my-6'
                                 />
                                 <TextField
                                     id='outlined-basic'
@@ -49,18 +49,18 @@ export default function Login() {
                                     label='Password'
                                     variant='outlined'
                                     margin='normal'
+                                    color='primary'
                                     value={password}
                                     onChange={passwordChange}
                                 />
-                                {/* <Button
-                                color='primary'
-                                variant='contained'
-                                size='large'
-                                >
-                                Login
-                                </Button> */}
-                                <button class='button is-primary'>Login</button>
-
+                                <div class='is-flex is-justify-content-space-between is-align-items-center'>
+                                    <label class="checkbox">
+                                        <input class='mr-1'type="checkbox"/>
+                                        Remember me
+                                    </label>
+                                    <p class='has-text-primary is-size-5'>Forgot Password?</p>
+                                </div>
+                                <button class='button is-primary my-6'>Login</button>
                             </div>
                         </div>
                     </div>
